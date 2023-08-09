a54c591da5af39a7814ef84f65cb18ace35d2520
azabelo <azabelo21old@gmail.com>
2023-08-09 15:23:10 -0700

    finetune

--- finetune.sh
+++ finetune.sh
@@ -38,3 +38,4 @@ OMP_NUM_THREADS=1 python -m torch.distributed.launch --nproc_per_node=${GPUS} \
     --dist_eval --test_num_segment 2 --test_num_crop 3 \
     --use_checkpoint \
     --enable_deepspeed
+    --num_workers 1
