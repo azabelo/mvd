607952ccfa97b144c39a2ad853b0f3427083de16
azabelo <azabelo21old@gmail.com>
2023-08-16 13:51:47 -0700

    experiments

--- finetune.sh
+++ finetune.sh
@@ -2,25 +2,27 @@
 
 # Check if four arguments are provided
 if [ $# -ne 8 ]; then
-    echo "please provide GPS NODE_COUNT RANK MASTER_PORT MASTER_ADDR (localhost when using only 1 GPU) BATCH_SIZE INITIAL_LR MODEL_PATH"
+    echo "please provide GPS MASTER_PORT MASTER_ADDR (localhost when using only 1 GPU) BATCH_SIZE LR MODEL_PATH EPCHOS UPDATE_FREQ NUM_SAMPLES"
     exit 1
 fi
 
 GPUS="$1"
-NODE_COUNT="$2"
-RANK="$3"
-MASTER_PORT="$4"
-MASTER_ADDR="$5"
-BATCH_SIZE="$6"
-LEARNING_RATE="$7"
+MASTER_PORT="$2"
+MASTER_ADDR="$3"
+BATCH_SIZE="$4"
+LEARNING_RATE="$5"
 OUTPUT_DIR='OUTPUT/mvd_vit_base_with_vit_base_teacher_HMDB51_finetune'
-MODEL_PATH="$8"
+MODEL_PATH="$6"
 DATA_PATH='finetune_splits'
 DATA_ROOT='hmdb51_mp4'
+EPOCHS="$7"
+UPDATE_FREQ="$8"
+NUM_SAMPLES="$9"
+
 
 OMP_NUM_THREADS=1 python -m torch.distributed.launch --nproc_per_node=${GPUS} \
-    --master_port ${MASTER_PORT} --nnodes=${NODE_COUNT} \
-    --node_rank=${RANK} --master_addr=${MASTER_ADDR} \
+    --master_port ${MASTER_PORT} --nnodes=1 \
+    --node_rank=0 --master_addr=${MASTER_ADDR} \
     run_class_finetuning.py \
     --model vit_base_patch16_224 \
     --data_set Kinetics-400 --nb_classes 51 \
@@ -31,10 +33,10 @@ OMP_NUM_THREADS=1 python -m torch.distributed.launch --nproc_per_node=${GPUS} \
     --output_dir ${OUTPUT_DIR} \
     --input_size 224 --short_side_size 224 \
     --opt adamw --opt_betas 0.9 0.999 --weight_decay 0.05 \
-    --batch_size ${BATCH_SIZE} --update_freq 1 --num_sample 2 \
-    --save_ckpt_freq 5 --no_save_best_ckpt \
+    --batch_size ${BATCH_SIZE} --update_freq ${UPDATE_FREQ} --num_sample ${NUM_SAMPLES} \
+    --save_ckpt_freq 20 --no_save_best_ckpt \
     --num_frames 16 \
-    --lr 5e-4 --epochs 10 \
+    --lr ${LEARNING_RATE} --epochs ${EPOCHS} \
     --dist_eval --test_num_segment 2 --test_num_crop 3 \
     --use_checkpoint \
     --enable_deepspeed
