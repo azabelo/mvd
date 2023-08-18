c86be134274fbd0ab7aa3e8ef7f07f9f7e0a0f21
azabelo <azabelo21old@gmail.com>
2023-08-17 22:58:47 -0700

    knn accuracy

--- run_mvd_pretraining.py
+++ run_mvd_pretraining.py
@@ -280,7 +280,7 @@ def main(args):
     sampler_val = torch.utils.data.SequentialSampler(dataset_val)
     data_loader_val = torch.utils.data.DataLoader(
     dataset_val, sampler=sampler_val,
-    batch_size=int(1.5 * args.batch_size),
+    batch_size=int(args.batch_size),
     num_workers=args.num_workers,
     pin_memory=args.pin_mem,
     drop_last=False
