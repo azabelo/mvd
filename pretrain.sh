5b48c72ee5f7365f222d717a4b05c9ab7ddb879f
azabelo <azabelo21old@gmail.com>
2023-08-18 19:31:33 -0700

    knn accuracy

--- pretrain.sh
+++ pretrain.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Check if four arguments are provided
-if [ $# -ne 9 ]; then
+if [ $# -ne 7 ]; then
     echo "please provide rid:GPS rid:MASTER_PORT rid:MASTER_ADDR (localhost when using only 1 GPU) BATCH_SIZE INITIAL_LR UPDATE_FREQ EPOCHS WARMUP SAMPLING_RATE USE_CLIP"
     exit 1
 fi
