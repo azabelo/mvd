1655f8659fd67f9d588cda2698c79a251fa28231
azabelo <azabelo21old@gmail.com>
2023-08-22 02:02:04 -0700

    test checkpointing

--- engine_for_pretraining.py
+++ engine_for_pretraining.py
@@ -22,75 +22,11 @@ import torch.nn.functional as F
 
 Loss_func_choice = {'L1': torch.nn.L1Loss, 'L2': torch.nn.MSELoss, 'SmoothL1': torch.nn.SmoothL1Loss}
 
-def knn_predict(
-    feature: Tensor,
-    feature_bank: Tensor,
-    feature_labels: Tensor,
-    num_classes: int,
-    knn_k: int = 199,
-    knn_t: float = 0.1,
-) -> Tensor:
-    """Run kNN predictions on features based on a feature bank
-
-    This method is commonly used to monitor performance of self-supervised
-    learning methods.
-
-    The default parameters are the ones
-    used in https://arxiv.org/pdf/1805.01978v1.pdf.
-
-    Args:
-        feature:
-            Tensor with shape (B, D) for which you want predictions.
-        feature_bank:
-            Tensor of shape (D, N) of a database of features used for kNN.
-        feature_labels:
-            Labels with shape (N,) for the features in the feature_bank.
-        num_classes:
-            Number of classes (e.g. `10` for CIFAR-10).
-        knn_k:
-            Number of k neighbors used for kNN.
-        knn_t:
-            Temperature parameter to reweights similarities for kNN.
-
-    Returns:
-        A tensor containing the kNN predictions
-
-
-    """
-    # compute cos similarity between each feature vector and feature bank ---> (B, N)
-    sim_matrix = torch.mm(feature, feature_bank)
-    # (B, K)
-    sim_weight, sim_indices = sim_matrix.topk(k=knn_k, dim=-1)
-    # (B, K)
-    sim_labels = torch.gather(
-        feature_labels.expand(feature.size(0), -1), dim=-1, index=sim_indices
-    )
-    sim_labels = sim_labels.long()
-    # we do a reweighting of the similarities
-    sim_weight = (sim_weight / knn_t).exp()
-    # counts for each class
-    one_hot_label = torch.zeros(
-        feature.size(0) * knn_k, num_classes, device=sim_labels.device
-    )
-    # (B*K, C)
-    one_hot_label = one_hot_label.scatter(
-        dim=-1, index=sim_labels.view(-1, 1), value=1.0
-    )
-    # weighted score ---> (B, C)
-    pred_scores = torch.sum(
-        one_hot_label.view(feature.size(0), -1, num_classes)
-        * sim_weight.unsqueeze(dim=-1),
-        dim=1,
-    )
-    pred_labels = pred_scores.argsort(dim=-1, descending=True)
-    return pred_labels[:, 0]
-
 def train_one_epoch(args, model: torch.nn.Module, data_loader: Iterable, optimizer: torch.optim.Optimizer,
                     device: torch.device, epoch: int, loss_scaler, max_norm: float = 0,
                     log_writer=None, lr_scheduler=None, start_steps=None, lr_schedule_values=None,
                     wd_schedule_values=None, update_freq=None, time_stride_loss=True, lr_scale=1.0,
                     image_teacher_model=None, video_teacher_model=None, norm_feature=False,data_for_knn=None,):
-    log_knn_acc(data_for_knn, model)
 
     model.train()
     metric_logger = utils.MetricLogger(delimiter="  ")
@@ -234,7 +170,7 @@ def train_one_epoch(args, model: torch.nn.Module, data_loader: Iterable, optimiz
     metric_logger.synchronize_between_processes()
     print("Averaged stats:", metric_logger)
 
-
+    log_knn_acc(data_for_knn, model)
 
     return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
 
@@ -315,3 +251,66 @@ def log_knn_acc(data_for_knn, model):
         lightly_knn_accuracy = accuracy_score(test_labels, pred_labels)
 
         wandb.log({"knn_accuracy_lightly": lightly_knn_accuracy, "knn_accuracy_custom": knn_accuracy_custom})
+
+def knn_predict(
+    feature: Tensor,
+    feature_bank: Tensor,
+    feature_labels: Tensor,
+    num_classes: int,
+    knn_k: int = 199,
+    knn_t: float = 0.1,
+) -> Tensor:
+    """Run kNN predictions on features based on a feature bank
+
+    This method is commonly used to monitor performance of self-supervised
+    learning methods.
+
+    The default parameters are the ones
+    used in https://arxiv.org/pdf/1805.01978v1.pdf.
+
+    Args:
+        feature:
+            Tensor with shape (B, D) for which you want predictions.
+        feature_bank:
+            Tensor of shape (D, N) of a database of features used for kNN.
+        feature_labels:
+            Labels with shape (N,) for the features in the feature_bank.
+        num_classes:
+            Number of classes (e.g. `10` for CIFAR-10).
+        knn_k:
+            Number of k neighbors used for kNN.
+        knn_t:
+            Temperature parameter to reweights similarities for kNN.
+
+    Returns:
+        A tensor containing the kNN predictions
+
+
+    """
+    # compute cos similarity between each feature vector and feature bank ---> (B, N)
+    sim_matrix = torch.mm(feature, feature_bank)
+    # (B, K)
+    sim_weight, sim_indices = sim_matrix.topk(k=knn_k, dim=-1)
+    # (B, K)
+    sim_labels = torch.gather(
+        feature_labels.expand(feature.size(0), -1), dim=-1, index=sim_indices
+    )
+    sim_labels = sim_labels.long()
+    # we do a reweighting of the similarities
+    sim_weight = (sim_weight / knn_t).exp()
+    # counts for each class
+    one_hot_label = torch.zeros(
+        feature.size(0) * knn_k, num_classes, device=sim_labels.device
+    )
+    # (B*K, C)
+    one_hot_label = one_hot_label.scatter(
+        dim=-1, index=sim_labels.view(-1, 1), value=1.0
+    )
+    # weighted score ---> (B, C)
+    pred_scores = torch.sum(
+        one_hot_label.view(feature.size(0), -1, num_classes)
+        * sim_weight.unsqueeze(dim=-1),
+        dim=1,
+    )
+    pred_labels = pred_scores.argsort(dim=-1, descending=True)
+    return pred_labels[:, 0]
