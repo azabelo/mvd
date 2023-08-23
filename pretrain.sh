c314682dec8918467e95363044522b20b11a7777
azabelo <azabelo21old@gmail.com>
2023-08-22 17:42:20 -0700

    test checkpointing

--- pretrain.sh
+++ pretrain.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Check if four arguments are provided
-if [ $# -ne 8 ]; then
+if [ $# -ne 9 ]; then
     echo "please provide GPS MASTER_PORT rid:MASTER_ADDR (localhost when using only 1 GPU) BATCH_SIZE INITIAL_LR UPDATE_FREQ EPOCHS WARMUP SAMPLING_RATE USE_CLIP"
     exit 1
 fi
