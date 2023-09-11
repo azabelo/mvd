ed5976cd9ceb767dd46d03921456978de98fdcb7
azabelo <azabelo21old@gmail.com>
2023-09-11 17:57:10 +0200

    restart from checkpoint

--- pretrain.sh
+++ pretrain.sh
@@ -40,7 +40,7 @@ OMP_NUM_THREADS=1 python3 -m torch.distributed.launch --nproc_per_node=${GPUS} \
         --video_distill_loss_func SmoothL1 \
         --video_teacher_model_ckpt_path 'video_teacher.pth' \
         --mask_type tube --mask_ratio 0.9 --decoder_depth 2 \
-        --batch_size ${BATCH_SIZE} --update_freq ${UPDATE_FREQ} --save_ckpt_freq 1 \
+        --batch_size ${BATCH_SIZE} --update_freq ${UPDATE_FREQ} --save_ckpt_freq 20 \
         --num_frames 16 --sampling_rate ${SAMPLING_RATE} \
         --lr ${LEARNING_RATE} --min_lr 1e-4 --drop_path 0.1 --warmup_epochs ${WARMUP} --epochs ${EPOCHS} \
         --use_cls_token \
