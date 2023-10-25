5d1c1693db5a514b94fdc97d510a874bccdbd101
azabelo <azabelo21old@gmail.com>
2023-10-25 00:32:20 -0700

    tiny_k400

--- pretrain.sh
+++ pretrain.sh
@@ -21,8 +21,9 @@ USE_CLIP="$9"
 
 
 OUTPUT_DIR='OUTPUT/mvd_vit_base_with_vit_base_teacher_HMDB51'
-DATA_PATH='train.csv'
+#DATA_PATH='train.csv'
 #DATA_ROOT='hmdb51_mp4'
+DATA_PATH='tiny_k400_train.csv'
 DATA_ROOT='tiny-Kinetics-400'
 
 OMP_NUM_THREADS=1 python3 -m torch.distributed.launch --nproc_per_node=${GPUS} \
