f8765fde700390e101b60fb8e5b93d193afb9595
azabelo <azabelo21old@gmail.com>
2023-11-01 23:13:00 -0700

    s3d

--- vid_test.py
+++ vid_test.py
@@ -1,3 +1,6 @@
 from s3d import S3D
+import torch
 
 video_encoder = S3D('pretrained_models/s3d_dict.npy', 512)
+video_encoder.load_state_dict(
+            torch.load('pretrained_models/s3d_howto100m.pth'))
