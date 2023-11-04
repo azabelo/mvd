b1cd8956207d0d7c687a2e85036e1dc90934f390
azabelo <azabelo21old@gmail.com>
2023-11-04 16:55:47 -0700

    fixing uninitialzes params

--- run_mvd_pretraining.py
+++ run_mvd_pretraining.py
@@ -423,9 +423,6 @@ warmup: {args.warmup_epochs}, sapling: {args.sampling_rate}"
         else:
             checkpoint = torch.load(args.video_teacher_model_ckpt_path, map_location='cpu')
 
-        checkpoint = checkpoint[0]
-        checkpoint = {key: value for key, value in checkpoint}
-
         print("Load video teacher ckpt from %s" % args.video_teacher_model_ckpt_path)
         checkpoint_model = None
         for model_key in args.model_key.split('|'):
