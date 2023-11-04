3dc1c8b6b1008440b6cbc1c81b2d8be7de177b15
azabelo <azabelo21old@gmail.com>
2023-11-04 13:21:45 -0700

    videomaev2 !

--- VideoMAEv2/run_mae_pretraining.py
+++ VideoMAEv2/run_mae_pretraining.py
@@ -24,11 +24,13 @@ from timm.models import create_model
 # NOTE: Do not comment `import models`, it is used to register models
 import VideoMAEv2.models  # noqa: F401
 import utils
-from dataset import build_pretraining_dataset
-from engine_for_pretraining import train_one_epoch
-from optim_factory import create_optimizer
-from utils import NativeScalerWithGradNormCount as NativeScaler
-from utils import multiple_pretrain_samples_collate
+# not needed if we just want to get the model:
+
+# from dataset import build_pretraining_dataset
+# from engine_for_pretraining import train_one_epoch
+# from optim_factory import create_optimizer
+# from utils import NativeScalerWithGradNormCount as NativeScaler
+# from utils import multiple_pretrain_samples_collate
 
 
 def get_argsv2():
@@ -269,187 +271,187 @@ def get_modelv2(args):
         model = torch.compile(model)
 
     return model
-
-
-def main(args):
-    utils.init_distributed_mode(args)
-
-    print(args)
-
-    device = torch.device(args.device)
-
-    # fix the seed for reproducibility
-    seed = args.seed + utils.get_rank()
-    torch.manual_seed(seed)
-    np.random.seed(seed)
-    random.seed(seed)
-    cudnn.benchmark = True
-
-    model = get_modelv2(args)
-    patch_size = model.encoder.patch_embed.patch_size
-    print("Patch size = %s" % str(patch_size))
-    args.window_size = (args.num_frames // args.tubelet_size,
-                        args.input_size // patch_size[0],
-                        args.input_size // patch_size[1])
-    args.patch_size = patch_size
-
-    # get dataset
-    dataset_train = build_pretraining_dataset(args)
-
-    num_tasks = utils.get_world_size()
-    global_rank = utils.get_rank()
-    sampler_rank = global_rank
-    total_batch_size = args.batch_size * num_tasks
-
-    num_training_steps_per_epoch = len(dataset_train) // total_batch_size
-
-    sampler_train = torch.utils.data.DistributedSampler(
-        dataset_train, num_replicas=num_tasks, rank=sampler_rank, shuffle=True)
-    print("Sampler_train = %s" % str(sampler_train))
-
-    if global_rank == 0 and args.log_dir is not None:
-        os.makedirs(args.log_dir, exist_ok=True)
-        log_writer = utils.TensorboardLogger(log_dir=args.log_dir)
-    else:
-        log_writer = None
-
-    if args.num_sample > 1:
-        collate_func = partial(multiple_pretrain_samples_collate, fold=False)
-    else:
-        collate_func = None
-
-    data_loader_train = torch.utils.data.DataLoader(
-        dataset_train,
-        sampler=sampler_train,
-        batch_size=args.batch_size,
-        num_workers=args.num_workers,
-        pin_memory=args.pin_mem,
-        drop_last=True,
-        collate_fn=collate_func,
-        worker_init_fn=utils.seed_worker,
-        persistent_workers=True)
-
-    if args.finetune:
-        checkpoint = torch.load(args.finetune, map_location='cpu')
-
-        print("Load ckpt from %s" % args.finetune)
-        checkpoint_model = None
-        for model_key in ['model', 'module']:
-            if model_key in checkpoint:
-                checkpoint_model = checkpoint[model_key]
-                print("Load state_dict by model_key = %s" % model_key)
-                break
-        if checkpoint_model is None:
-            checkpoint_model = checkpoint
-
-        utils.load_state_dict(model, checkpoint_model)
-
-    model.to(device)
-    model_without_ddp = model
-    n_parameters = sum(p.numel() for p in model.parameters()
-                       if p.requires_grad)
-
-    print("Model = %s" % str(model_without_ddp))
-    print('number of params: {} M'.format(n_parameters / 1e6))
-
-    # scale the lr
-    args.lr = args.lr * total_batch_size / 256
-    args.min_lr = args.min_lr * total_batch_size / 256
-    args.warmup_lr = args.warmup_lr * total_batch_size / 256
-
-    print("LR = %.8f" % args.lr)
-    print("Batch size = %d" % total_batch_size)
-    print("Number of training steps = %d" % num_training_steps_per_epoch)
-    print("Number of training examples per epoch = %d" %
-          (total_batch_size * num_training_steps_per_epoch))
-
-    if args.distributed:
-        model = torch.nn.parallel.DistributedDataParallel(
-            model, device_ids=[args.gpu], find_unused_parameters=False)
-        model_without_ddp = model.module
-
-    optimizer = create_optimizer(args, model_without_ddp)
-    loss_scaler = NativeScaler()
-
-    print("Use step level LR & WD scheduler!")
-    lr_schedule_values = utils.cosine_scheduler(
-        args.lr,
-        args.min_lr,
-        args.epochs,
-        num_training_steps_per_epoch,
-        warmup_epochs=args.warmup_epochs,
-        warmup_steps=args.warmup_steps,
-    )
-    if args.weight_decay_end is None:
-        args.weight_decay_end = args.weight_decay
-    wd_schedule_values = utils.cosine_scheduler(args.weight_decay,
-                                                args.weight_decay_end,
-                                                args.epochs,
-                                                num_training_steps_per_epoch)
-    print("Max WD = %.7f, Min WD = %.7f" %
-          (max(wd_schedule_values), min(wd_schedule_values)))
-
-    utils.auto_load_model(
-        args=args,
-        model=model,
-        model_without_ddp=model_without_ddp,
-        optimizer=optimizer,
-        loss_scaler=loss_scaler)
-    torch.cuda.empty_cache()
-    print(f"Start training for {args.epochs} epochs")
-    start_time = time.time()
-    for epoch in range(args.start_epoch, args.epochs):
-        if args.distributed:
-            data_loader_train.sampler.set_epoch(epoch)
-        if log_writer is not None:
-            log_writer.set_step(epoch * num_training_steps_per_epoch)
-        train_stats = train_one_epoch(
-            model,
-            data_loader_train,
-            optimizer,
-            device,
-            epoch,
-            loss_scaler,
-            args.clip_grad,
-            log_writer=log_writer,
-            start_steps=epoch * num_training_steps_per_epoch,
-            lr_schedule_values=lr_schedule_values,
-            wd_schedule_values=wd_schedule_values,
-            patch_size=patch_size[0],
-            normlize_target=args.normlize_target)
-        if args.output_dir:
-            _epoch = epoch + 1
-            if _epoch % args.save_ckpt_freq == 0 or _epoch == args.epochs:
-                utils.save_model(
-                    args=args,
-                    model=model,
-                    model_without_ddp=model_without_ddp,
-                    optimizer=optimizer,
-                    loss_scaler=loss_scaler,
-                    epoch=epoch)
-
-        log_stats = {
-            **{f'train_{k}': v
-               for k, v in train_stats.items()}, 'epoch': epoch,
-            'n_parameters': n_parameters
-        }
-
-        if args.output_dir and utils.is_main_process():
-            if log_writer is not None:
-                log_writer.flush()
-            with open(
-                    os.path.join(args.output_dir, "log.txt"),
-                    mode="a",
-                    encoding="utf-8") as f:
-                f.write(json.dumps(log_stats) + "\n")
-
-    total_time = time.time() - start_time
-    total_time_str = str(datetime.timedelta(seconds=int(total_time)))
-    print('Training time {}'.format(total_time_str))
-
-
-if __name__ == '__main__':
-    opts = get_argsv2()
-    if opts.output_dir:
-        Path(opts.output_dir).mkdir(parents=True, exist_ok=True)
-    main(opts)
+#
+#
+# def main(args):
+#     utils.init_distributed_mode(args)
+#
+#     print(args)
+#
+#     device = torch.device(args.device)
+#
+#     # fix the seed for reproducibility
+#     seed = args.seed + utils.get_rank()
+#     torch.manual_seed(seed)
+#     np.random.seed(seed)
+#     random.seed(seed)
+#     cudnn.benchmark = True
+#
+#     model = get_modelv2(args)
+#     patch_size = model.encoder.patch_embed.patch_size
+#     print("Patch size = %s" % str(patch_size))
+#     args.window_size = (args.num_frames // args.tubelet_size,
+#                         args.input_size // patch_size[0],
+#                         args.input_size // patch_size[1])
+#     args.patch_size = patch_size
+#
+#     # get dataset
+#     dataset_train = build_pretraining_dataset(args)
+#
+#     num_tasks = utils.get_world_size()
+#     global_rank = utils.get_rank()
+#     sampler_rank = global_rank
+#     total_batch_size = args.batch_size * num_tasks
+#
+#     num_training_steps_per_epoch = len(dataset_train) // total_batch_size
+#
+#     sampler_train = torch.utils.data.DistributedSampler(
+#         dataset_train, num_replicas=num_tasks, rank=sampler_rank, shuffle=True)
+#     print("Sampler_train = %s" % str(sampler_train))
+#
+#     if global_rank == 0 and args.log_dir is not None:
+#         os.makedirs(args.log_dir, exist_ok=True)
+#         log_writer = utils.TensorboardLogger(log_dir=args.log_dir)
+#     else:
+#         log_writer = None
+#
+#     if args.num_sample > 1:
+#         collate_func = partial(multiple_pretrain_samples_collate, fold=False)
+#     else:
+#         collate_func = None
+#
+#     data_loader_train = torch.utils.data.DataLoader(
+#         dataset_train,
+#         sampler=sampler_train,
+#         batch_size=args.batch_size,
+#         num_workers=args.num_workers,
+#         pin_memory=args.pin_mem,
+#         drop_last=True,
+#         collate_fn=collate_func,
+#         worker_init_fn=utils.seed_worker,
+#         persistent_workers=True)
+#
+#     if args.finetune:
+#         checkpoint = torch.load(args.finetune, map_location='cpu')
+#
+#         print("Load ckpt from %s" % args.finetune)
+#         checkpoint_model = None
+#         for model_key in ['model', 'module']:
+#             if model_key in checkpoint:
+#                 checkpoint_model = checkpoint[model_key]
+#                 print("Load state_dict by model_key = %s" % model_key)
+#                 break
+#         if checkpoint_model is None:
+#             checkpoint_model = checkpoint
+#
+#         utils.load_state_dict(model, checkpoint_model)
+#
+#     model.to(device)
+#     model_without_ddp = model
+#     n_parameters = sum(p.numel() for p in model.parameters()
+#                        if p.requires_grad)
+#
+#     print("Model = %s" % str(model_without_ddp))
+#     print('number of params: {} M'.format(n_parameters / 1e6))
+#
+#     # scale the lr
+#     args.lr = args.lr * total_batch_size / 256
+#     args.min_lr = args.min_lr * total_batch_size / 256
+#     args.warmup_lr = args.warmup_lr * total_batch_size / 256
+#
+#     print("LR = %.8f" % args.lr)
+#     print("Batch size = %d" % total_batch_size)
+#     print("Number of training steps = %d" % num_training_steps_per_epoch)
+#     print("Number of training examples per epoch = %d" %
+#           (total_batch_size * num_training_steps_per_epoch))
+#
+#     if args.distributed:
+#         model = torch.nn.parallel.DistributedDataParallel(
+#             model, device_ids=[args.gpu], find_unused_parameters=False)
+#         model_without_ddp = model.module
+#
+#     optimizer = create_optimizer(args, model_without_ddp)
+#     loss_scaler = NativeScaler()
+#
+#     print("Use step level LR & WD scheduler!")
+#     lr_schedule_values = utils.cosine_scheduler(
+#         args.lr,
+#         args.min_lr,
+#         args.epochs,
+#         num_training_steps_per_epoch,
+#         warmup_epochs=args.warmup_epochs,
+#         warmup_steps=args.warmup_steps,
+#     )
+#     if args.weight_decay_end is None:
+#         args.weight_decay_end = args.weight_decay
+#     wd_schedule_values = utils.cosine_scheduler(args.weight_decay,
+#                                                 args.weight_decay_end,
+#                                                 args.epochs,
+#                                                 num_training_steps_per_epoch)
+#     print("Max WD = %.7f, Min WD = %.7f" %
+#           (max(wd_schedule_values), min(wd_schedule_values)))
+#
+#     utils.auto_load_model(
+#         args=args,
+#         model=model,
+#         model_without_ddp=model_without_ddp,
+#         optimizer=optimizer,
+#         loss_scaler=loss_scaler)
+#     torch.cuda.empty_cache()
+#     print(f"Start training for {args.epochs} epochs")
+#     start_time = time.time()
+#     for epoch in range(args.start_epoch, args.epochs):
+#         if args.distributed:
+#             data_loader_train.sampler.set_epoch(epoch)
+#         if log_writer is not None:
+#             log_writer.set_step(epoch * num_training_steps_per_epoch)
+#         train_stats = train_one_epoch(
+#             model,
+#             data_loader_train,
+#             optimizer,
+#             device,
+#             epoch,
+#             loss_scaler,
+#             args.clip_grad,
+#             log_writer=log_writer,
+#             start_steps=epoch * num_training_steps_per_epoch,
+#             lr_schedule_values=lr_schedule_values,
+#             wd_schedule_values=wd_schedule_values,
+#             patch_size=patch_size[0],
+#             normlize_target=args.normlize_target)
+#         if args.output_dir:
+#             _epoch = epoch + 1
+#             if _epoch % args.save_ckpt_freq == 0 or _epoch == args.epochs:
+#                 utils.save_model(
+#                     args=args,
+#                     model=model,
+#                     model_without_ddp=model_without_ddp,
+#                     optimizer=optimizer,
+#                     loss_scaler=loss_scaler,
+#                     epoch=epoch)
+#
+#         log_stats = {
+#             **{f'train_{k}': v
+#                for k, v in train_stats.items()}, 'epoch': epoch,
+#             'n_parameters': n_parameters
+#         }
+#
+#         if args.output_dir and utils.is_main_process():
+#             if log_writer is not None:
+#                 log_writer.flush()
+#             with open(
+#                     os.path.join(args.output_dir, "log.txt"),
+#                     mode="a",
+#                     encoding="utf-8") as f:
+#                 f.write(json.dumps(log_stats) + "\n")
+#
+#     total_time = time.time() - start_time
+#     total_time_str = str(datetime.timedelta(seconds=int(total_time)))
+#     print('Training time {}'.format(total_time_str))
+#
+#
+# if __name__ == '__main__':
+#     opts = get_argsv2()
+#     if opts.output_dir:
+#         Path(opts.output_dir).mkdir(parents=True, exist_ok=True)
+#     main(opts)
