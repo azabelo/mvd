8c74d7991210182d630a0f27e63a3446086bd650
azabelo <azabelo21old@gmail.com>
2023-11-04 22:25:55 -0700

    fixing uninitialzes params

--- vid_test.py
+++ vid_test.py
@@ -5,7 +5,7 @@ video_encoder = S3D('pretrained_models/s3d_dict.npy', 512)
 video_encoder.load_state_dict(
             torch.load('pretrained_models/s3d_howto100m.pth'))
 
-input_tensor = torch.rand(1, 3, 32, 224, 224)
+input_tensor = torch.rand(2, 3, 32, 224, 224)
 
 print(video_encoder)
 
