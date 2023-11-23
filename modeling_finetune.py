59a95a2dccdd1d934075a3de05ac0df9ef7eb488
azabelo <azabelo21old@gmail.com>
2023-11-22 16:21:31 -0800

    fixing v2

--- modeling_finetune.py
+++ modeling_finetune.py
@@ -408,9 +408,12 @@ class VisionTransformer(nn.Module):
 
     def forward(self, x):
         # Check if the input tensor is not of type torch.cuda.HalfTensor
-        if not (torch.is_floating_point(x) and x.dtype == torch.float16):
+        if x.dim() == 6:
             print("features")
-            return self.forward_features(x.half())
+            return self.forward_features(x.squeeze(0).half())
+        # if not (torch.is_floating_point(x) and x.dtype == torch.float16):
+        #     print("features")
+        #     return self.forward_features(x.half())
         print("head")
         x = self.forward_features(x)
         x = self.head(self.fc_dropout(x))
