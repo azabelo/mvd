5a78fe71758a4a090f35572475f93e2cf59e36ac
azabelo <azabelo21old@gmail.com>
2023-11-22 16:07:44 -0800

    fixing v2

--- engine_for_finetuning.py
+++ engine_for_finetuning.py
@@ -38,9 +38,8 @@ def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
                     num_training_steps_per_epoch=None, update_freq=None,
                     zero_shot_blyat=False, clip_model=None, vision_encoder=None, text_features=None, prompts=None, args=None,data_loader_val2=None):
 
-
-    engine_for_pretraining.log_knn_acc(data_loader_val2, model, finetuning=True)
-
+    with torch.no_grad():
+        engine_for_pretraining.log_knn_acc(data_loader_val2, model, finetuning=True)
 
     ######## for zero shot   #######
 
